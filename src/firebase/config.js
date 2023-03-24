93f076f43ff7bfa78313765af09b03a728039a14
leonbahley <leon.bahley@icloud.com>
2023-03-24 11:01:20 +0100

    add-gitignore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,6 @@ import "firebase/auth";
 import "firebase/storage";
 import "firebase/firestore";
 import { REACT_APP_FIREBASE_KEY } from "@env";
-console.log("REACT_APP_FIREBASE_KEY", REACT_APP_FIREBASE_KEY);
 
 const firebaseConfig = {
   apiKey: REACT_APP_FIREBASE_KEY,
